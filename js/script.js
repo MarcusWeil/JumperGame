4ecd338ce718a3e4f5b107e739f7ff3c8eee4548
Marcus Vinícius Weil <vinicciusplay10@gmail.com>
2022-10-20 21:10:11 -0300

    Game Over initial replace

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const marioCharacter = document.querySelector('.mario');
+const gameOver = document.querySelector('.game-over');
 const pipeProp = document.querySelector('.pipe');
 
 const jump = () => {
-    loop();
     marioCharacter.classList.add('jump');
     
     setTimeout(() => {       
@@ -10,10 +10,25 @@ const jump = () => {
     },800)
 }
 
-const loop = () => {
-    setInterval(() => {
+const loop = setInterval(() => {
         const pipePosition = pipeProp.offsetLeft;
-        console.log(pipePosition)
+        // Foi necessário usar windowGetComputedStyle porque a propriedade
+        // offsetBottom não exis te, e assim é possível pegar qualquer elemento
+        const marioPosition = window.getComputedStyle(marioCharacter).bottom.replace('px','');
+
+        if(pipePosition > 0 && pipePosition <= 110 && marioPosition < 110.0){
+            pipeProp.style.animation = 'none'
+            pipeProp.style.left = `${pipePosition}px`;
+            marioCharacter.style.animation = 'none'
+            marioCharacter.style.bottom = `${marioPosition}px`;
+            gameOver.style.bottom = `${marioPosition - 20}px`;
+
+            marioCharacter.classList.add('hidden')
+            gameOver.classList.remove('hidden')
+
+            clearInterval(loop);
+
+        }
     },10)
-}
+
 document.addEventListener('keydown', jump);
