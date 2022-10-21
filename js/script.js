3eef119571e1b92767532bdb2b3877134c291794
Marcus Vinícius Weil <vinicciusplay10@gmail.com>
2022-10-21 12:35:49 -0300

    Score

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const marioCharacter = document.querySelector('.mario');
 const gameOver = document.querySelector('.game-over');
 const pipeProp = document.querySelector('.pipe');
+let Gamescore = 0;
+let highscore;
 
 const jump = () => {
     marioCharacter.classList.add('jump');
@@ -11,10 +13,16 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
-        const pipePosition = pipeProp.offsetLeft;
-        // Foi necessário usar windowGetComputedStyle porque a propriedade
-        // offsetBottom não exis te, e assim é possível pegar qualquer elemento
-        const marioPosition = window.getComputedStyle(marioCharacter).bottom.replace('px','');
+    const pipePosition = pipeProp.offsetLeft;
+    // Foi necessário usar windowGetComputedStyle porque a propriedade
+    // offsetBottom não exis te, e assim é possível pegar qualquer elemento
+    const marioPosition = window.getComputedStyle(marioCharacter).bottom.replace('px','');
+    
+    const score = document.querySelector(".score h4").innerHTML = Gamescore.toFixed(0);
+    Gamescore += 1/5;
+
+        
+
 
         if(pipePosition > 0 && pipePosition <= 100 && marioPosition < 100){
             pipeProp.style.animation = 'none'
